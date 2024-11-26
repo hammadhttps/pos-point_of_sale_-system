0121e6551b44fe11931f4de7061ee2070f410f1b
ahsan-hacker69 <ahsan.ahmad2004@gmail.com>
2024-11-27 00:07:53 +0500

    Controller classes made of Branch Manager and Super Admin.

---
+++ src/main/java/controller/BranchManager.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+
+public class BranchManager {
+
+
+}
---
+++ src/main/java/controller/SuperAdmin.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class SuperAdmin {
+    Button button1;
+    Button button2;
+    public void createBranch(ActionEvent e)
+    {
+
+    }
+}
